701e38fa5f3a5ef4fe4ee549f6a46e13023dfdb4
Arvind Arya <aryan.singh4198@gmail.com>
2026-04-09 23:05:50 +0530

    changes done

--- p1.java
+++ p1.java
@@ -2,7 +2,7 @@ public class p1
 {
  public static void main(String[] args) 
  {
-  int n=11;
+  int n=23;
   for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
@@ -32,6 +32,30 @@ public class p1
         for(int j=0;j<n;j++)
         {
             if(i==0||j==0||i==n-1||j==n-1||i-j==(n-1)/2||i+j==(n-1)/2||j-i==(n-1)/2||i+j==(n-1)/2+n-1)
+            {
+                System.out.print("*");
+            }
+            else
+            {
+                System.out.print(" ");
+            }
+        }
+        System.out.print("        ");
+        for(int j=0;j<n;j++)
+        {
+            if(i==0||j==0||i==n-1||j==n-1||i-j==(n-1)/2||i+j==(n-1)/2||j-i==(n-1)/2||i+j==(n-1)/2+n-1||i==j||i+j==n-1)
+            {
+                System.out.print("*");
+            }
+            else
+            {
+                System.out.print(" ");
+            }
+        }
+        System.out.print("        ");
+        for(int j=0;j<n;j++)
+        {
+            if(i==0||j==0||i==n-1||j==n-1||i-j>=(n-1)/2||i+j<=(n-1)/2||j-i>=(n-1)/2||i+j>=(n-1)/2+n-1)
             {
                 System.out.print("*");
             }
